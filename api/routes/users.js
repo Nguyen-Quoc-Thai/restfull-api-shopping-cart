63c56e3520d6062f741f633b03d40404a1155a51
quocthai <ct1.nquocthai@gmail.com>
2020-09-08 16:12:29 +0700

    Adding user signin

--- api/routes/users.js
+++ api/routes/users.js
@@ -4,6 +4,9 @@ const bcrypt = require('bcrypt')
 
 const User = require('./../../models/user')
 
+const baseUrl = process.env.BASE_URL || 'http://localhost:8080'
+const usersUrl = baseUrl + '/users/'
+
 const router = express.Router()
 
 router.post('/signup', (req, res, next) => {
@@ -56,6 +59,76 @@ router.post('/signup', (req, res, next) => {
     .catch(error => {
         res.status(500).json({
             msg: "Server error!",
+            error
+        })
+    })
+})
+
+router.post('/login', (req, res, next) => {
+    const {email, password} = req.body
+
+    if(!email || !password){
+        return res.status(404).json({
+            msg: 'Email and password are required!'
+        })
+    }
+
+    User.find({email})
+    .then(user => {
+        if(user.length < 1){
+            return res.status(404).json({
+                msg: 'Auth failed!'
+            })
+
+        }else{
+            bcrypt.compare(password, user[0].password, (error, matched) => {
+                if(error){
+                    return res.status(404).json({
+                        msg: 'Auth failed!'
+                    })
+
+                }else{
+                    if(matched){
+                        return res.status(200).json({
+                            msg: 'Auth successfully!'
+                        })
+                    }
+                }
+
+                res.status(404).json({
+                    msg: 'Auth failed!'
+                })
+            })
+        }
+    })
+    .catch(error => {
+        res.status(500).json({
+            msg: 'Server error!',
+            error
+        })
+    })
+})
+
+router.delete('/:userID', (req, res, next) => {
+    const {userID: _id} = req.params
+
+    User.remove({_id})
+    .then(result => {
+        res.status(200).json({
+            msg: 'success',
+            request: {
+                type: 'POST',
+                url: usersUrl + 'signup',
+                body:{
+                    email: "String",
+                    password: "String"
+                }
+            }
+        })
+    })
+    .catch(error => {
+        res.status(500).json({
+            msg: 'Server error!',
             error
         })
     })
